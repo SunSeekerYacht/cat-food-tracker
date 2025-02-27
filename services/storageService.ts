7821a649c0d1452639ecf80d743f383464c3d2c9
Angelo <59173505+SunSeekerYacht@users.noreply.github.com>
2025-02-27 16:34:55 +0100

    add firebase integration

--- services/storageService.ts
+++ services/storageService.ts
@@ -1,22 +1,6 @@
 import { Meal } from '@/types/meal';
 import { collection, addDoc, getDocs, deleteDoc } from "firebase/firestore";
-import { getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
-    appId: process.env.FIREBASE_APP_ID
-};
-
-const app = initializeApp(firebaseConfig)
-
-const db = getFirestore(app);
-
-export { db }
+import { db } from '@/services/firebase';
 
 
 export class StorageAPI {
